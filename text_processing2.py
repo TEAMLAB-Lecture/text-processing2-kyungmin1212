8fc8842f040ad2cc397e2490de4f1a1a0969a8e0
kyungmin1212 <uiuiui12@naver.com>
2021-01-19 17:20:00 +0900

    Update my code

--- text_processing2.py
+++ text_processing2.py
@@ -28,7 +28,7 @@ def digits_to_words(input_string):
             >>> tp2.digits_to_words(digits_str2)
             'three one four one five'
     """
-    number_dic={"0":"one","1":"one","2":"two","3":"three","4":"four","5":"five","6":"six","7":"seven","8":"eight","9":"nine"}
+    number_dic={"0":"zero","1":"one","2":"two","3":"three","4":"four","5":"five","6":"six","7":"seven","8":"eight","9":"nine"}
     result=[]
     for i in input_string:
         if i in number_dic.keys():
@@ -90,7 +90,7 @@ def to_camel_case(underscore_str):
 
     result=underscore_str.split("_")
     new_result=[]
-    
+
     for i in result:
         if i !="":
             new_result.append(i)
